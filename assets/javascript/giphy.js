822d3e8f24d499de09d76008d78b969078175844
Michelle Ocampo <manneocampo@gmail.com>
2017-10-04 19:41:38 -0400

    added new code

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,21 +1,77 @@
-var topics = ["will smith", "ellen degeneres", "whoopi goldberg"]
+var topics = ["hermione granger", "ron weasley", "harry potter", "dumbledore", "voldemort", "ginny"];
 
-$("button").on("click",function() {
-	var person = $(this).attr("data-person");
+function displayTopicInfo() {
+		
 
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=dc6zaTOxFJmzC&limit=10";
+		var person = $(this).attr("data-topic");
 
-    $ajax.({
-    	url:queryURL, 
-    	method: "GET"
-    })
-    .done(function(response) {
-    	var results = response.data;
+		//constructing URL to search Giphy for name of data-topic
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+	        person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-    	for (var i=0; i < results.length; i++) {
-    		if(results[i].rating !== "r" && results[i].rating !== "pg-13");
-    	} 
+	    $.ajax({
+	    	url:queryURL, 
+	    	method: "GET"
+	    })
+	    .done(function(response) {
+	    	console.log(response);
 
-    });
-})
+	    	var results = response.data;
+
+	    	for (var i=0; i < results.length; i++) {
+	    		
+	    		if(results[i].rating !== "r" && results[i].rating !== "pg-13"){
+
+	    			var gifDiv = $("<div class='item'>");
+
+	    			var rating = results[i].rating;
+
+	    			var p =$("<p>").text("Rating:" + rating); 
+
+	    			var personImage = $("<img>"); 
+
+	    			personImage.attr("src", results[i].images.fixed_height.url);
+
+	    			gifDiv.append(p);
+	    			gifDiv.append(personImage);
+
+	    			$("#gifs-appear-here").prepend(gifDiv);
+	    			
+	    		}
+	    	} 
+
+	    });
+	};
+
+	function renderButtons () {
+		$("#buttons-view").empty();
+
+		for (var i = 0; i < topics.length; i++) {
+			var arrayTopics = $("<button>");
+
+			arrayTopics.addClass("topic");
+
+			arrayTopics.attr("data-topic", topics[i]);
+
+			arrayTopics.text(topics[i]);
+
+			$("buttons-view").append(arrayTopics);
+		}
+	}
+
+	$("#add-gif").on("click", function(event){
+		event.preventDefault();
+
+		var gif = $("#gif-input").val().trim();
+
+		topics.push(gif);
+
+		renderButtons();
+	});
+
+
+	//event listener for all button elements
+	$(document).on("click", ".topic", displayTopicInfo); 
+	renderButtons(); 
+
+	
